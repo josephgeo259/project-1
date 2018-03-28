32647cb74ca98930b526791b49b82bacb8c83eb6
GeoJoseph <josephgeo259@gmail.com>
2018-03-27 22:15:09 -0400

    javascript adds

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,34 @@
 var words = ['tray', 'hand', 'star', 'bear', 'fort', 'nail'];
+var maxAttempts = 6;
+var rightGuesses= [];
+var wrongGuesses= [];
+var guess;
 var secretWord;
-var underscoreWord = []
-
+var underscoreWord = [];
+var failedCounter;
 var randomInt = Math.floor(Math.random() * words.length)
 console.log(randomInt)
+
 var secretWord = words[randomInt];
 console.log(secretWord)
+var splitWord = secretWord.split("")
+console.log(splitWord)
 
 
-splitWord = function () {
-    console.log(secretWord.split(""))
+console.log(typeof guesses )
+
+
+// check if the guess is right 
+rightGuess = function (){
+for (i=0;i > splitWord.length;i++){
+    if(guess=== splitWord[i]){
+rightGuesses.push (guess);
+    }
 }
+}    
+ 
+//if right push up to right array
 
-splitWord()
+//if wrong push to wrong array 
 
 
