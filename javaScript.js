e652b69d0b8b403dbc1201a0000c6151144c292d
GeoJoseph <josephgeo259@gmail.com>
2018-03-28 23:51:04 -0400

    win or lose alert or prompt

--- javaScript.js
+++ javaScript.js
@@ -25,19 +25,25 @@ $("#dashs").append(underscoreWord);
 
 $(document).ready(function () {
     $("button").on("click", function () {
-        letterPicked = ($(event.target).attr('id'));
-        document.getElementById(letterPicked).disabled = true;
-
-        alert(letterPicked.toUpperCase() + " was the letter clicked.");
-        console.log(letterPicked + " clickevent");
-        // evaluteUserInput(letterPicked)
+      
+        buttonDisable();
         wrongInput();
         rightInput();
-        
-     
+        loserMessage();
+        winnerMessage();
 
     });
 });
+
+// Adding to UnderScore
+
+
+function buttonDisable(){
+    letterPicked = ($(event.target).attr('id'));
+    document.getElementById(letterPicked).disabled = true;
+    alert(letterPicked.toUpperCase() + " was the letter clicked.");
+    console.log(letterPicked + " clickevent");
+}
 // checking if there are wrong inputs and taking away a life 
 function wrongInput(){
     console.log((secretWord.indexOf(letterPicked) === -1)+ " checking if this is a wrong pick")
@@ -61,6 +67,20 @@ function rightInput() {
         }
         $("#dashs").text(underscoreWord.join(" "))
 
+    }
+}
+function loserMessage() {
+    if(maxAttempts===0){
+        alert("You Lose, Please Refresh the page to Play Again")
+        $("button").attr("disabled", "disable")
+    }
+};
+
+function winnerMessage(){
+    console.log(" Secret Word: " +secretWord + "   "+underscoreWord.join("") +" \n To String \n ")
+    if (secretWord === (underscoreWord.join(""))){
+        alert("You Win, Please Refresh the page to Play Again")
+        $("button").attr("disabled", "disable")
     }
 }
 
